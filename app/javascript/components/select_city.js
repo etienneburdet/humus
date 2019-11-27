c316fa12bb726d96a2af60e4265fe9c73f03a012
Etienne <etienne.burdet@gmail.com>
2019-11-27 15:59:11 +0100

    autocomplete search

--- app/javascript/components/select_city.js
+++ app/javascript/components/select_city.js
@@ -1,10 +1,9 @@
-import $ from 'jquery';
-import 'select2';
+import places from 'places.js';
 
 const initAutocomplete = () => {
-  if ($('.city-ajax')) {
-    const cities = ["Amsterdam","Bali","Barcelona","Belo Horizonte","Berlin","Bordeaux","Brussels","Buenos Aires","Casablanca","Chengdu","Copenhagen","Kyoto","Lausanne","Lille","Lisbon","London","Lyon","Madrid","Marseille","Melbourne","Mexico","Milan","Montréal","Nantes","Oslo","Paris","Rio de Janeiro","Rennes","Rome","São Paulo","Seoul","Shanghai","Shenzhen","Singapore","Stockholm","Tel Aviv","Tokyo"];
-    $('.city-ajax').select2({ data: cities });
+  const addressInput = document.getElementById('q');
+  if (addressInput) {
+    places({ container: addressInput });
   }
 };
 
