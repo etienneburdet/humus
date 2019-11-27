05d5ca80cb6126bd1edc466b0d72b898a26ab7ff
Etienne <etienne.burdet@gmail.com>
2019-11-27 17:52:55 +0100

    fixed -> need to update btn in partials cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,5 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
-import 'select2/dist/css/select2.css'
 
 import { initAutocomplete }    from '../components/select_city.js'
 import { createProjectMapBox } from '../components/create_project_mapbox.js'
