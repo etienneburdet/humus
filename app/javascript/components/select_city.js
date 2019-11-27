9be8824c3fb4b322e8d286919c9128e74f67f13b
Etienne <etienne.burdet@gmail.com>
2019-11-27 16:18:54 +0100

    algolia search html broken

--- app/javascript/components/select_city.js
+++ app/javascript/components/select_city.js
@@ -3,7 +3,8 @@ import places from 'places.js';
 const initAutocomplete = () => {
   const addressInput = document.getElementById('q');
   if (addressInput) {
-    places({ container: addressInput });
+    places({ container: addressInput })
+      .configure({ type: 'city' });
   }
 };
 
