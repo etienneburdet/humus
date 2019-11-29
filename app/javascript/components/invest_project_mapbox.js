31ad41deb878983c5c3446e89a8057998500af08
Etienne <etienne.burdet@gmail.com>
2019-11-29 12:35:24 +0100

    see marker on map

--- app/javascript/components/invest_project_mapbox.js
+++ app/javascript/components/invest_project_mapbox.js
@@ -3,9 +3,9 @@ import mapboxgl from 'mapbox-gl';
 const investProjectMapBox = () => {
   const mapElement = document.getElementById('investProjectMap');
 
-  const fitMapToMarkers = (map, markers) => {
+  const fitMapToMarker = (map, marker) => {
     const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    bounds.extend([ marker.lng, marker.lat ]);
     map.fitBounds(bounds, { padding: 100, maxZoom: 15, duration: 0 });
   };
 
@@ -16,16 +16,11 @@ const investProjectMapBox = () => {
       style: 'mapbox://styles/mapbox/streets-v11'
     });
 
-    const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow)
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup)
-        .addTo(map);
-    });
-
-    fitMapToMarkers(map, markers);
+    const marker = JSON.parse(mapElement.dataset.marker);
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
+    fitMapToMarker(map, marker);
   }
 };
 
