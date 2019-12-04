b5bce303331bde5d2204f05a73b0409161bdba36
Silvia Patono <silvia.patono@gmail.com>
2019-12-04 17:27:18 +0100

    repush

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'search', to: 'projects#search', as: :search
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :projects, except: %i[edit update destroy] do
     resources :contracts, only: %i[new create]
