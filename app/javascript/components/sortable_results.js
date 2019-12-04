9205f3d6b47ef1114bbe45d3e375e1da89ceb0ee
Etienne <etienne.burdet@gmail.com>
2019-12-04 11:11:32 +0100

    sort first column on header click

--- app/javascript/components/sortable_results.js
+++ app/javascript/components/sortable_results.js
@@ -3,13 +3,22 @@ import { Sortable } from '@shopify/draggable';
 const initSortable = () => {
 
   const sortable = new Sortable(document.querySelectorAll('tbody'), {
-    draggable: 'tr'
+    draggable: 'tr',
   });
 
-  // sortable.on('sortable:start', () => console.log('sortable:start'));
-  // sortable.on('sortable:sort', () => console.log('sortable:sort'));
-  // sortable.on('sortable:sorted', () => console.log('sortable:sorted'));
-  // sortable.on('sortable:stop', () => console.log('sortable:stop'));
-}
+  const tableBody = document.querySelector('tbody')
+  const rows = Array.from(document.querySelectorAll('tbody > tr'));
+  const headers = Array.from(document.querySelectorAll('thead > tr > th'))
+
+  const compareRows = (row1, row2) => {
+    console.log('Hello from sort rows');
+    const diff = row2.children[1].innerText - row1.children[1].innerText;
+    return diff;
+  };
+
+  const sortRows = (e) => rows.sort(compareRows).forEach( tr => tableBody.appendChild(tr));
+
+  headers.forEach( th => th.addEventListener('click', sortRows));
+};
 
 export { initSortable };
